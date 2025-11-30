7a17d56413ea7b21336958e943d474e42a33192c
Ryan Whetstone <whetstone.ryan@gmail.com>
2025-11-30 01:22:54 -0500

    Add image hostname configuration for iNaturalist photos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'inaturalist-open-data.s3.amazonaws.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
