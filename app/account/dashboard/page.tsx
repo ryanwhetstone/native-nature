117046e8c7f75c398db2720f18d2d1d0c0f771be
Ryan Whetstone <whetstone.ryan@gmail.com>
2025-11-30 01:34:16 -0500

    Show 6 most recent favorites on dashboard

--- app/account/dashboard/page.tsx
+++ app/account/dashboard/page.tsx
@@ -36,6 +36,7 @@ export default async function DashboardPage() {
     with: {
       species: true,
     },
+    orderBy: (favorites, { desc }) => [desc(favorites.createdAt)],
     limit: 6,
   });
 
